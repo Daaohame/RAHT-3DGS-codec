0a6d0a1f6adaf94ac769320f6b44cfa69e51c179
Daaohame <haodongw@usc.edu>
2025-09-26 18:16:08 -0700

    Apply inverse RAHT and verify reconstructed colors

--- matlab/encode_ply.m
+++ matlab/encode_ply.m
@@ -5,7 +5,7 @@ addpath(genpath('RAHT'));
 
 % List of PLY file paths
 ply_paths = {'/ssd1/haodongw/workspace/3dstream/3DGS_Compression_Adaptive_Voxelization/attributes_compressed/train_depth_15_thr_30_3DGS_adapt_lossless/train_dc.ply'};
-J = 15;
+J = 18;
 
 T = length(ply_paths);
 Nvox = zeros(T,1);
@@ -18,9 +18,6 @@ for frame =1:T
     N = size(V,1);
     Nvox(frame) = N;
     C = Crgb; % shape [N x k]
-
-    disp(size(V));
-    disp(size(C));
     
     % ListC: indices of colors
     % FlagsC: indicator of whether a node is a left sibling to another node
@@ -28,12 +25,16 @@ for frame =1:T
     [ListC,FlagsC,weightsC]=RAHT_param(V,[0 0 0], 2^J, J);
     
     [Coeff,w]=RAHT(C,ListC,FlagsC,weightsC);
-    [~,IX_ref] = sort(w,'descend');
-    
-    Y = Coeff(:,1);
+    C_recon = iRAHT(Coeff, ListC, FlagsC, weightsC);
 
-    fprintf('Energy of C: %.4f\n', norm(C));
-    fprintf('Energy of Coeff: %.4f\n', norm(Coeff));
+    if ismembertol(C, C_recon, 1e-8)
+        fprintf('Reconstruction check: PASSED (within tolerance)\n');
+    else
+        fprintf('Reconstruction check: FAILED (difference exceeds tolerance)\n');
+    end
+
+    fprintf('L2-norm of C: %.4f\n', norm(C));
+    fprintf('L2-norm of Coeff: %.4f\n', norm(Coeff));
     time(frame)=toc;
     disp([time(frame) frame T])
 end
